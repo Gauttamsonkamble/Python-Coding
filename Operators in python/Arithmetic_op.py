c090bf87a9e4ccde4b8e975db6150dcbf51951ac
Gauttam Sonkamble <gauttamsonkamble358@gmail.com>
2025-11-16 23:33:46 +0530

    Arithmetic Operators in Python

---
+++ Operators in python/Arithmetic_op.py
@@ -0,0 +1,23 @@
+
+# print(5 // 2)
+
+a = 5
+
+b = 2
+
+c = a + b
+
+sub = a - b
+
+mult = a * b
+
+div = a / b
+
+print("Addition = ",c)
+
+print(" Sub = ",sub)
+
+print("Mult = ",mult)
+
+print("Div = ",div)
+
