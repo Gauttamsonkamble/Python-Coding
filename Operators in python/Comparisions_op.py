414eccdacd70c3b08138dd3de63d71350d8280d8
Gauttam Sonkamble <gauttamsonkamble358@gmail.com>
2025-11-20 08:20:48 +0530

    comparisions operator in python

---
+++ Operators in python/Comparisions_op.py
@@ -0,0 +1,26 @@
+
+# print( 5 == 5)
+
+# print (5 != 3)
+
+# print ( 5 > 3)
+
+# print ( 3 < 5)
+
+# print( 5 >= 5)
+
+# print( 3 <= 5)
+
+a = 5 
+
+b = 10 
+
+print( a > b)
+
+print( a == b)
+
+print(a >= b )
+
+print( a < b)
+
+print
