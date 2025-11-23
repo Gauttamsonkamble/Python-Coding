a306f11985a9bba50d127953d47af861bcc23cad
Gauttam Sonkamble <gauttamsonkamble358@gmail.com>
2025-11-23 08:19:29 +0530

    Bitwise Operator in Python

---
+++ Operators in python/Bitwise_op.py
@@ -0,0 +1,14 @@
+
+# Bitwise Operator
+
+print( "And : ", 5 & 3)  # And 
+
+print("OR : ", 5 | 3)  # OR
+
+print("XOR : ", 5 ^ 3)  # XOR
+
+print("NOT : ", ~5)     # NOT
+
+print( "Left Shift : ", 5 << 1)
+
+print(" Rigt Shift : ", 5 >> 1)
