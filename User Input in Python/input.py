f0f9da6f0cbecd674a6be8c5004cdf89f10304d0
Gauttam Sonkamble <gauttamsonkamble358@gmail.com>
2025-11-29 10:23:35 +0530

    input functions

---
+++ User Input in Python/input.py
@@ -0,0 +1,13 @@
+
+
+# name = "Gauttam"
+name2 = input("Enter Your name : ")
+# print("Name : ",name2)
+
+num1 = int(input("Enter Number 1 : "))
+num2 = float(input(" Enter Number 2 :"))
+# print(type(num1))
+
+sum = num1 + num2
+print("Addition = ",sum)
+
