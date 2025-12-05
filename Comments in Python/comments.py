f09b6db19a77656cf219c335ece401e65f5b7247
Gauttam Sonkamble <gauttamsonkamble358@gmail.com>
2025-12-05 22:59:42 +0530

    Comments in python

---
+++ Comments in Python/comments.py
@@ -0,0 +1,20 @@
+
+
+# addition of two numbers
+
+# Gauttam Has written this code
+
+a = 10 
+
+b = 20 
+
+print(a + b)
+
+
+""" addition of two numbers
+
+Gauttam Has written this code sgjkljgag
+amgkamgamk """
+
+''' Developercorners has prioviding online 
+Training and vidoes in their Youtube channel '''
