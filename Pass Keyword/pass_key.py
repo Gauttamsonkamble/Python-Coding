24c315da4495e2231e3d1897531c5058a8d12fed
Gauttam Sonkamble <gauttamsonkamble358@gmail.com>
2025-12-15 10:49:42 +0530

    pass keyword in python

---
+++ Pass Keyword/pass_key.py
@@ -0,0 +1,17 @@
+
+# age = 18
+
+# if age >= 18:
+#     # pass
+#     print("Your eligible")
+# else:
+#     print("Not Eligible")
+
+for i in range(6):
+    pass
+
+def myFunction():
+    pass
+
+class Student:
+    pass
