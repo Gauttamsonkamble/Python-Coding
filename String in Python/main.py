94376f2b5c6ce189645786ad61f98fc6bf640b7f
Gauttam Sonkamble <gauttamsonkamble358@gmail.com>
2025-12-16 09:47:03 +0530

    String in Python

---
+++ String in Python/main.py
@@ -0,0 +1,18 @@
+
+name = "Python"
+
+city = 'Mumbai'
+
+message = ''' Welcome in Python Programming Training'''
+
+Channel = """ DeveloperCorners"""
+
+print(name)
+
+print(city)
+
+print(message)
+
+print(Channel)
+
+print(name,city,message,Channel)
