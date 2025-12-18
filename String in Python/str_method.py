ff1afce8fd8c8dd1bcca9eb22e74f01d98738ad0
Gauttam Sonkamble <gauttamsonkamble358@gmail.com>
2025-12-18 10:48:46 +0530

    String Methods in Python

---
+++ String in Python/str_method.py
@@ -0,0 +1,13 @@
+
+text = "I love Python"
+
+# print(text.find("Python"))
+
+# print(text.replace("love","Like"))
+
+# print(text.split())
+
+# print(text.upper())
+
+for char in "Python":
+    print(char)
