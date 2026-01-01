b2d202e849a7b280471d799b9a87817e7d4c2cd8
Gauttam Sonkamble <gauttamsonkamble358@gmail.com>
2026-01-01 08:25:43 +0530

    Sets in Python

---
+++ Data Structure in Python/sets/set1.py
@@ -0,0 +1,8 @@
+
+my_set = {1,2,3,4,5}
+
+print(my_set)
+
+my_set2 = set([1,2,3,4,3])
+
+print(my_set2)
---
+++ Data Structure in Python/sets/set_function.py
@@ -0,0 +1,21 @@
+
+set1 = {1,2,3}
+
+print(set1)
+
+set1.add(4)
+
+print(set1)
+
+set1.remove(1)
+set1.discard(5)
+
+print(set1)
+
+set1.pop()
+
+print(set1)
+
+set1.clear()
+
+print(set1)
