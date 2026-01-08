130597f7b44016a18dbd2ad09bc6f0131af86b92
Gauttam Sonkamble <gauttamsonkamble358@gmail.com>
2026-01-08 22:28:40 +0530

    set comprehension in python

---
+++ Data Structure in Python/sets/set_cmpr2.py
@@ -0,0 +1,10 @@
+
+# even_numbers = {x for x in range(1,11) if x % 2 == 0}
+
+# print(even_numbers)
+
+word = "programming"
+
+unique_letters = {ch for ch in word}
+
+print(unique_letters)
---
+++ Data Structure in Python/sets/set_comprehension.py
@@ -0,0 +1,6 @@
+
+numbers = [1,2,3,4,5,5]
+
+square = {x * x for x in numbers}
+
+print(square)
