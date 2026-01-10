7cdc55ccf4f78e038a6faf963d05b986e91f9182
Gauttam Sonkamble <gauttamsonkamble358@gmail.com>
2026-01-11 00:01:58 +0530

    Nested Tuple in Python

---
+++ Data Structure in Python/tuples/Nested Tuple/access_tuple.py
@@ -0,0 +1,8 @@
+
+data = (1,2,(3,4,5),6)
+
+print(data[0])
+
+print(data[2])
+
+print(data[2][1])
---
+++ Data Structure in Python/tuples/Nested Tuple/nested_tuple.py
@@ -0,0 +1,8 @@
+
+simple_tuple = (10,20,30,40,50)
+
+nested_tuple = (10,20,(30,40),50)
+
+print(simple_tuple)
+
+print(nested_tuple)
