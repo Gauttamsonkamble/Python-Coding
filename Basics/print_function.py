f31ac23ce6b82848fdb18c267995425493aa6d9c
Gauttam Sonkamble <gauttamsonkamble358@gmail.com>
2026-01-12 09:05:22 +0530

    print function in Python

---
+++ Basics/print_function.py
@@ -0,0 +1,38 @@
+
+# a = 10
+# b = 20
+
+# c = a + b
+
+# print(c)
+
+# print("Hello , world")
+
+# print(10)
+
+# print(5 + 10)
+
+name = "Gauttam"
+
+age = 20
+
+# print(name)
+
+# print(age)
+
+# print("Age is",25)
+
+# print("2026","01","12",sep="/")
+
+# print("Hello",end=" ")
+
+# print("Python")
+
+# name = "Python"
+
+# version = 3.13
+
+# print(f"{name} version is {version}")
+
+for i in range(5):
+    print(i)
