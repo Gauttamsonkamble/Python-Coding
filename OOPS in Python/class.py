264807e2ee34e6bcbe22f27bb8e770e1e4e7058a
Gauttam Sonkamble <gauttamsonkamble358@gmail.com>
2026-01-22 23:33:19 +0530

    OOPS in Python

---
+++ OOPS in Python/class.py
@@ -0,0 +1,11 @@
+
+class Student:
+    name = "Gauttam"
+    age = 29
+
+s1 = Student()
+
+print(s1.name)
+
+print(s1.age)
+
---
+++ OOPS in Python/class2.py
@@ -0,0 +1,12 @@
+
+class Students:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+
+s1 = Students("Rahul",25)
+
+print(s1.name)
+print(s1.age)
+        
