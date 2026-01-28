26049e7b802765b2fca76a16b015483b33559396
Gauttam Sonkamble <gauttamsonkamble358@gmail.com>
2026-01-29 00:18:47 +0530

    Method Oveloading in Python

---
+++ OOPS in Python/Method Overloading/main.py
@@ -0,0 +1,11 @@
+
+class Math:
+    def add(self,a,b):
+        return a + b
+    
+    def add(self,a,b,c):
+        return a + b + c
+    
+m = Math()
+
+print(m.add(5,6,2))
---
+++ OOPS in Python/Method Overloading/main2.py
@@ -0,0 +1,8 @@
+class cal:
+    def add(self, a, b, c = 0):
+        return a + b + c
+    
+c = cal()
+
+print(c.add(5,6,7))
+print(c.add(10,20))
